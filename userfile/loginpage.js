d4038c1fd80d25569afb97cc89562d4c6bc5f9ad
Harsha643 <battuharsha6@gmail.com>
2025-02-09 18:20:40 +0530

    90

--- userfile/loginpage.js
+++ userfile/loginpage.js
@@ -1,80 +1,78 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getAuth,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-import { getFirestore, getDoc,doc} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBbmBJPwfOKa2Q-r2CEhvsFv5yhmNFkuAU",
-    authDomain: "login-kk-f580d.firebaseapp.com",
-    projectId: "login-kk-f580d",
-    storageBucket: "login-kk-f580d.firebasestorage.app",
-    messagingSenderId: "901441715555",
-    appId: "1:901441715555:web:5228323a42259e1e91a756"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBbmBJPwfOKa2Q-r2CEhvsFv5yhmNFkuAU",
+  authDomain: "login-kk-f580d.firebaseapp.com",
+  projectId: "login-kk-f580d",
+  storageBucket: "login-kk-f580d.firebasestorage.app",
+  messagingSenderId: "901441715555",
+  appId: "1:901441715555:web:5228323a42259e1e91a756"
+};
 
-  const auth=getAuth()
-  const db=getFirestore()
-  onAuthStateChanged(auth, (user)=>{
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-    const loggedInUserId=localStorage.getItem("loggedInUserId")
-    if(loggedInUserId){
-        // console.log(user.email)
-        // console.log(loggedInUserId)
-        const docRef=doc(db, "users",loggedInUserId)
-        // console.log(docRef)
-        getDoc(docRef)
-        .then((docSnap)=>{
-            // console.log(docSnap.data())
-            if(docSnap.exists()){
+const auth = getAuth()
+const db = getFirestore()
+onAuthStateChanged(auth, (user) => {
 
-                const userData=docSnap.data()
-                // console.log(userData)
-                document.getElementById("username").innerText=userData.username
-                document.getElementById("email").innerText=userData.email
-                document.getElementById("password").innerText=userData.password
-                // document.getElementById("id").innerText=
-                
+  const loggedInUserId = localStorage.getItem("loggedInUserId")
+  if (loggedInUserId) {
+    // console.log(user.email)
+    // console.log(loggedInUserId)
+    const docRef = doc(db, "users", loggedInUserId)
+    // console.log(docRef)
+    getDoc(docRef)
+      .then((docSnap) => {
+        // console.log(docSnap.data())
+        if (docSnap.exists()) {
 
-            }
-            else{
-                console.log("no document found matching id ")
-            }
-
-        })
-        .catch((error)=>{
-            console.error("error geting ",error)
-        })
-
-    }
-    else{
-        console.error("user id not found in local storage")
-    }
-  })
-
-  let logout=document.getElementById("logout")
-  logout.addEventListener("click",(e)=>{
-    e.preventDefault()
-    window.location.href="signin.html"
-  })
-
-  //userprofile
+          const userData = docSnap.data()
+          // console.log(userData)
+          document.getElementById("username").innerText = userData.username
+          document.getElementById("email").innerText = userData.email
+          document.getElementById("password").innerText = userData.password
+          // document.getElementById("id").innerText=
 
 
-let profileContainer=document.getElementById("profileContainer")
-profileContainer.style.display="none"
-  let profile=document.getElementById("userprofile")
-  profile.addEventListener("click",(e)=>{
-    e.preventDefault()
+        }
+        else {
+          console.log("no document found matching id ")
+        }
 
-    profileContainer.style.display=profileContainer.style.display==="none"?"block":"none"
-   
+      })
+      .catch((error) => {
+        console.error("error geting ", error)
+      })
+
+  }
+  else {
+    console.error("user id not found in local storage")
+  }
+})
+
+let logout = document.getElementById("logout")
+logout.addEventListener("click", (e) => {
+  e.preventDefault()
+  window.location.href = "signin.html"
+})
+
+//userprofile
+let profileContainer = document.getElementById("profileContainer")
+profileContainer.style.display = "none"
+let profile = document.getElementById("userprofile")
+profile.addEventListener("click", (e) => {
+  e.preventDefault()
+  profileContainer.style.display = profileContainer.style.display === "none" ? "block" : "none"
+  profileContainer.style.width="250px"
+
+})
 
 
-  })
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////// fetching data
 async function fetchingData() {
   try {
     let response = await fetch("https://pouncing-scarlet-cesium.glitch.me/meals");
@@ -84,7 +82,7 @@ async function fetchingData() {
     let data = await response.json();
     displayData1(data);
     // console.log(data)
-   
+
   } catch (err) {
     alert("Data fetch failed");
     console.error(err);
@@ -92,116 +90,142 @@ async function fetchingData() {
 }
 fetchingData();
 
+// displayrecipes
 function displayData1(data) {
   const categoryData = {};
 
+  const countryData = {};
+
+
   data.forEach((element) => {
-
-
+    // categoryData appending
     if (element.Category) {
       if (!categoryData[element.Category]) {
         categoryData[element.Category] = [];
       }
       categoryData[element.Category].push(element);
     } else {
-     
+
       console.warn("Element found without a category:", element);
     }
+
+    // countrydata appending
+    if (element.Area) {
+      // console.log(element.Area)
+      if (!countryData[element.Area]) {
+        countryData[element.Area] = [];
+      }
+      countryData[element.Area].push(element);
+    } else {
+
+      console.warn("Element found without a category:", element);
+    }
+
+
+
   });
 
-  const container = document.getElementById("maincontainer");
-  container.innerHTML = ``;
+  catdata(categoryData)
 
 
-  for (const category in categoryData) {
-    // console.log(category)
-    const categoryContainer = document.createElement("div");
-    let catName=document.createElement("div")
-    catName.classList.add("catName");
-    catName.innerHTML=`<h2>${category}</h2>`
-    container.appendChild(catName)
-    categoryContainer.classList.add("category-container");
-    
-    categoryData[category].forEach((element) => {
-      const item = document.createElement("div");
-      item.classList.add("meal-item"); 
-    
+  let container = document.getElementById("maincontainer")
+  let containerA = document.getElementById("main2container")
 
-      item.innerHTML = `
-        <img src="${element.image}" width="200px" class="imgopen" data-id="${element.id}">
-        <p>${element.mealName}</p>
-         <span id="fava" class="favorite-icon"><ion-icon name="heart-outline"></ion-icon></span>
-      `;
-      categoryContainer.append(item);
-      const favoriteIcon = item.querySelector(".favorite-icon");
-      favoriteIcon.addEventListener("click", () => {
+  let countryContainer = document.getElementById("footcountry");
+  countryContainer.style.backgroundColor = "#ccc"
+  countryContainer.style.height = "200px"
 
-        const favoriteMeals = JSON.parse(localStorage.getItem("favoriteMeals")) || [];
-        const isAlreadyFavorite = favoriteMeals.some((meal) => meal.id === element.id);
-        if (!isAlreadyFavorite) {
-          favoriteMeals.push(element);
-          localStorage.setItem("favoriteMeals", JSON.stringify(favoriteMeals));
-          favoriteIcon.querySelector("ion-icon").name = "heart";
-        } else {
-          const filteredFavorites = favoriteMeals.filter((meal) => meal.id !== element.id);
-          localStorage.setItem("favoriteMeals", JSON.stringify(filteredFavorites));
-          favoriteIcon.querySelector("ion-icon").name = "heart-outline";
+
+  if (countryContainer) {
+    let allAreas = [];
+    for (const country in countryData) {
+      countryData[country].forEach(element => {
+        if (!allAreas.includes(element.Area)) {
+          allAreas.push(element.Area);
         }
-
       });
+    }
+    // console.log(allAreas)
+    const uniqueAreas = [...new Set(allAreas)];
     
-   
-    container.appendChild(categoryContainer);
+    uniqueAreas.forEach((element) => {
+       
+      const items = document.createElement("h5");
+      items.classList.add(`ele-${data.id}`);
+      items.textContent = element || "No Name";
+      let  countryFilter=[]
+
+      // click the country
+      items.addEventListener("click", () => {
+console.log("hello")
+containerA.innerHTML=""              
+        const filteredData =countryData[element];
+        filteredData.forEach((ele) => {
+         
+     
+          let filterdiv = document.createElement("div")
   
-  if (Object.keys(categoryData).length === 0) {
-    container.innerHTML = "No meals found.";
-  }
+          
+          if(element==ele.Area){
+            countryFilter.push(ele.Area)
 
-        // image 
-
-      let imgOpen = item.querySelector(".imgopen");
-  imgOpen.addEventListener("click", (event1) => {
-    event1.preventDefault();
-
-    console.log("Image clicked for meal ID:", element)
-  localStorage.setItem("imageitem",JSON.stringify(element))
-    window.location.href="./image.html"
-
-  });
+          }
 
 
+          filterdiv.innerHTML =
+            `
+         <img src="${ele.image}" width="200px" class="imgopen" data-id="${ele.id}"/>
+         <h3>${ele.mealName}</h3>
+         `
+          // container.style.display = container.style.display==="block"? "none":"none";
 
+          containerA.append(filterdiv )
+
+        })
+        // console.log(countryFilter)
+       
+        filcon(countryFilter)
+      });
+      countryContainer.appendChild(items);
     });
 
-   
-    container.appendChild(categoryContainer);
-  }
 
-  if (Object.keys(categoryData).length === 0) {
-    container.innerHTML = "No meals found.";
-  }
 
+    // console.log(data)
+  }
+  else {
+    console.error("Element with ID 'footcountry' not found.");
+  }
 }
 
-let favopen=document.getElementById("favopen")
-favopen.addEventListener("click",()=>{
-  window.location.href="fav.html"
+
+// fav
+let favopen = document.getElementById("favopen")
+favopen.addEventListener("click", () => {
+  window.location.href = "fav.html"
 
 })
+
+
+
 
 
 // recipes adding
 
 let addRecipes = document.getElementById("addBtn");
+
 let containerInputs = document.getElementById("container-inputs");
 
 addRecipes.addEventListener("click", () => {
-  containerInputs.style.display = containerInputs.style.display==="none"? "grid" :"none";
+  containerInputs.style.display = containerInputs.style.display === "none" ? "grid" : "none";
   // containerInputs.style.display = "grid"; // Set grid layout
   containerInputs.style.width = "80%";
   containerInputs.style.marginTop = "50px";
   containerInputs.style.gridTemplateColumns = "repeat(2, 1fr)";
 });
+
+
+
 
 
 function valid() {
@@ -276,11 +300,50 @@ function clearForm() {
   });
 }
 
+// draft data
+
+let draftdata=document.getElementById("draftdata")
+draftdata.addEventListener("click",()=>{
+  dSave()
+  console.log("he")
+})
+async function dSave() {
+  console.log("hello")
+  if (!valid()) {
+    alert("Please fill out all fields correctly.");
+    return;
+  }
+  let recipeId = document.getElementById("id").value;
+  let obj = {
+    mealName: document.getElementById("mealname").value,
+    Category: document.getElementById("category").value,
+    Area: document.getElementById("country").value,
+    instructions: document.getElementById("Instructions").value,
+    image: document.getElementById("imageUrl").value,
+    ingredients: document.getElementById("ingredient").value.split("\n"),
+  };
+  try {
+    let method = "POST";
+    let response = await fetch("https://amplified-hulking-snowdrop.glitch.me/meals", {
+      method,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(obj),
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    alert(recipeId ? "Data updated successfully" : "Data saved successfully");
+    fetchingData(); 
+    clearForm(); 
+  } catch (error) {
+    console.error("Save data error", error);
+  }
+}
 
 
 // savedata 
-let savedata=document.getElementById("savedata")
-savedata.addEventListener("click",()=>{
+let savedata = document.getElementById("savedata")
+savedata.addEventListener("click", () => {
   dataSave()
 })
 async function dataSave() {
@@ -301,7 +364,7 @@ async function dataSave() {
 
   try {
     let method = "POST";
-   
+
 
     let response = await fetch("https://pouncing-scarlet-cesium.glitch.me/meals", {
       method,
@@ -314,80 +377,33 @@ async function dataSave() {
     }
 
     alert(recipeId ? "Data updated successfully" : "Data saved successfully");
-    fetchingData(); 
-    clearForm(); 
+    fetchingData();
+    clearForm();
   } catch (error) {
     console.error("Save data error", error);
   }
 }
 
 
-
-// draft data
-
-
-let draftdata=document.getElementById("draftdata")
-draftdata.addEventListener("click",()=>{
-  dSave()
-  console.log("he")
-})
-async function dSave() {
-  console.log("hello")
-
-  if (!valid()) {
-    alert("Please fill out all fields correctly.");
-    return;
-  }
-
-  let recipeId = document.getElementById("id").value;
-  let obj = {
-    mealName: document.getElementById("mealname").value,
-    Category: document.getElementById("category").value,
-    Area: document.getElementById("country").value,
-    instructions: document.getElementById("Instructions").value,
-    image: document.getElementById("imageUrl").value,
-    ingredients: document.getElementById("ingredient").value.split("\n"),
-  };
-
-  try {
-    let method = "POST";
-    let response = await fetch("https://amplified-hulking-snowdrop.glitch.me/meals", {
-      method,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(obj),
-    });
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-
-    alert(recipeId ? "Data updated successfully" : "Data saved successfully");
-    fetchingData(); 
-    clearForm(); 
-  } catch (error) {
-    console.error("Save data error", error);
-  }
-}
-
-
-
+// search function
 const filterInput = document.getElementById("search");
 
 filterInput.addEventListener("input", () => {
-  const filterText = filterInput.value.toLowerCase().trim(); 
-  const mealItems = document.querySelectorAll(".meal-item"); 
+  console.log("hello")
+  const filterText = filterInput.value.toLowerCase().trim();
+  const mealItems = document.querySelectorAll(".meal-item");
 
   mealItems.forEach((mealItem) => {
-    const mealName = mealItem.querySelector("p").textContent.toLowerCase(); 
-    const categoryElement = mealItem.querySelector("p:nth-child(2)"); 
-    const category = categoryElement ? categoryElement.textContent.toLowerCase() : ""; 
-    const countryElement = mealItem.querySelector("p:nth-child(3)"); 
-    const country = countryElement ? countryElement.textContent.toLowerCase() : ""; 
+    const mealName = mealItem.querySelector("h4").innerText.toLowerCase();
+    const categoryElement = mealItem.querySelector("h4:nth-child(2)");
+    const category = categoryElement ? categoryElement.innerText.toLowerCase() : "";
+    const countryElement = mealItem.querySelector("h4:nth-child(3)");
+    const country = countryElement ? countryElement.innerText.toLowerCase() : "";
 
     if (mealName.includes(filterText) || category.includes(filterText) || country.includes(filterText)) {
-      mealItem.style.display = "block"; 
+      mealItem.style.display = "block";
     } else {
-      mealItem.style.display = "none"; 
+      mealItem.style.display = "none";
     }
   });
 
@@ -396,9 +412,9 @@ filterInput.addEventListener("input", () => {
   categoryHeaders.forEach((header) => {
     const categoryName = header.textContent.toLowerCase();
     if (categoryName.includes(filterText)) {
-      header.style.display = "block"; 
+      header.style.display = "block";
     } else {
-      header.style.display = "none"; 
+      header.style.display = "none";
     }
   });
 });
@@ -417,3 +433,104 @@ filterInput.addEventListener("input", () => {
 
 
 // imageopen 
+
+
+
+
+
+// category recipes
+function catdata(categoryData) {
+  // console.log(categoryData)// console.log(categoryData)
+  const container = document.getElementById("maincontainer");
+  const subcontainer2=document.getElementById("subcontainer2")
+  subcontainer2.innerHTML = ``;
+
+  // categotydata  
+  for (const category in categoryData) {
+    // console.log(category)
+    const categoryContainer = document.createElement("div");
+    let catName = document.createElement("div")
+    catName.classList.add("catName");
+    catName.innerHTML = `<h2>${category}</h2>`
+    subcontainer2.appendChild(catName)
+    categoryContainer.classList.add("category-container");
+
+    categoryData[category].forEach((element) => {
+      const item = document.createElement("div");
+      item.classList.add("meal-item");
+
+
+      item.innerHTML = `
+        <img src="${element.image}" width="200px" class="imgopen" data-id="${element.id}">
+        <h4>${element.mealName}</h4>
+         <span id="fava" class="favorite-icon"><ion-icon name="heart-outline"></ion-icon></span>
+      `;
+      categoryContainer.append(item);
+      const favoriteIcon = item.querySelector(".favorite-icon");
+      favoriteIcon.addEventListener("click", () => {
+
+        const favoriteMeals = JSON.parse(localStorage.getItem("favoriteMeals")) || [];
+        const isAlreadyFavorite = favoriteMeals.some((meal) => meal.id === element.id);
+        if (!isAlreadyFavorite) {
+          favoriteMeals.push(element);
+          localStorage.setItem("favoriteMeals", JSON.stringify(favoriteMeals));
+          favoriteIcon.querySelector("ion-icon").name = "heart";
+        } else {
+          const filteredFavorites = favoriteMeals.filter((meal) => meal.id !== element.id);
+          localStorage.setItem("favoriteMeals", JSON.stringify(filteredFavorites));
+          favoriteIcon.querySelector("ion-icon").name = "heart-outline";
+        }
+      });
+
+
+      subcontainer2.appendChild(categoryContainer);
+
+      if (Object.keys(categoryData).length === 0) {
+        subcontainer2.innerHTML = "No meals found.";
+      }
+
+      // click image  details
+      let imgOpen = item.querySelector(".imgopen");
+      imgOpen.addEventListener("click", (event1) => {
+        event1.preventDefault();
+
+        console.log("Image clicked for meal ID:", element)
+        localStorage.setItem("imageitem", JSON.stringify(element))
+        window.location.href = "./image.html"
+
+      });
+
+
+
+    });
+
+
+    subcontainer2.appendChild(categoryContainer);
+  }
+
+  if (Object.keys(categoryData).length === 0) {
+   subcontainer2.innerHTML = "No meals found.";
+  }
+
+}
+
+
+// displaying country recipes
+function filcon(countryFilter){
+  // console.log(countryFilter)
+
+  let singlecountry=[...new Set(countryFilter)]
+  let single = singlecountry.join(", "); 
+  let inner=document.getElementById("inner")
+  inner.innerHTML = ""; 
+  inner.style.padding="30px"
+
+    console.log(single)
+    let filterh1=document.createElement("h1")
+  filterh1.innerText=`${single} recipes`
+
+    inner.appendChild(filterh1)
+
+}
+
+         
